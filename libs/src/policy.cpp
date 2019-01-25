64e10a26c9a05abd446f51047e848291f23a777d
Máté Kelemen <kelemen.mate.12@gmail.com>
2019-01-25 13:31:29 +0100

    modified the policy example

--- libs/src/policy.cpp
+++ libs/src/policy.cpp
@@ -1,65 +1,93 @@
 #include <iostream>
-
-
+#include <cmath>
+const double pi = 3.141592653589793238462643383279502884;
 
 /* -----------------------------------------------------------
 BASIC EXAMPLE FOR POLICY BASED DESIGN, TO ACCOMPANY THE THEORY
-- Several models of A/C machines with different output powers
-- Different units of measurement
-Task: we need to get the power output of any machine in any
-units of measurements.
 ----------------------------------------------------------- */
 
 
 
+// Derived classes are the hosts
+// Host class: sets the requirements for policies and combines them
+template <class policy_geometry, class policy_material>
+class physicsBody : private policy_geometry, private policy_material {
 
-// Derived template class -> inherits from two other template classes (policies)
-template <class policy_machine, class policy_units>
-class ACpower : public policy_machine, public policy_units {
-	// Member function
 public:
-	void getVal() {
-		// Using functions implemented in the base classes
-		std::cout 
-			<< policy_units::unitOfPower(	policy_machine::powerOutput()	)
-			<< "\n";
+	physicsBody(double characteristicLength) : policy_geometry(characteristicLength) {}
+	double mass() {
+		return policy_geometry::volume() * policy_material::density();
 	}
+
+};
+
+
+// Base classes represent policies
+// Policy: implements the requirements of the host class
+// Geometry policies --------------------------------------------------
+
+
+class sphere {
+private:
+	double radius_;
+public:
+	sphere(double radius) : radius_(radius) {}
+	double volume() { return 4 / 3 * radius_*radius_*radius_*pi; }
+};
+
+
+class cube {
+private:
+	double edge_;
+public:
+	cube(double edge) : edge_(edge) {}
+	double volume() { return edge_ * edge_ * edge_; }
+};
+
+
+// Material policies --------------------------------------------------
+
+
+class aluminum {
+private:
+	double density_;
+public:
+	aluminum() : density_(2700) {}
+	double density() { return density_; }
+};
+
+
+class plastic {
+private:
+	double density_;
+protected:
+	plastic() : density_(1700) {}
+	double density() { return density_; }
 };
 
 
 
 
 
-// Machine base class: Siemens machine -----------------------------------------
-struct siemensMachine {
-	double powerOutput() { return 2200; } // Power output in watts
-};
-
-// Machine base class: GE machine
-struct geMachine {
-	double powerOutput() { return 1500; } // Power output in watts
-};
-
-// Unit base class: metric (kilowatts) -----------------------------------------
-struct metricUnits {
-	double unitOfPower(double watts) { return watts / 1000; } // Return input power in kilowatts
-};
-
-// Unit base class: imperial (tons)
-struct imperialUnits {
-	double unitOfPower(double watts) { return watts / 3504; } // Return input power in tons
-};
 
 
 
 
-// Main
+
+// Main ---------------------------------------------------------------
 int main() {
-	// Create instances
-	ACpower<siemensMachine, metricUnits> pow1;
-	ACpower<geMachine, imperialUnits> pow2;
+	// Create an instance of the host object
+	physicsBody<cube, aluminum> body1(1);
+	std::cout << body1.mass() << std::endl;
 
-	// Call the function of the derived class
-	pow1.getVal();
-	pow2.getVal();
+
+
+	// Create a type of body
+	typedef physicsBody<sphere, plastic> billiardBall;
+	// Create an instance of a type of body
+	billiardBall body2(0.03);
+	std::cout << body2.mass() << std::endl;
+
+
+	return 0;
 }
